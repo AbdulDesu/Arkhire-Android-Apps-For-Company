0c83fa711d10504fe0b4769860f1a6f96e81ebd8
AbdulDesu <49888665+AbdulDesu@users.noreply.github.com>
2021-01-29 12:59:03 +0700

    fix screen orientation and force to light theme

--- app/src/main/java/com/sizdev/arkhireforcompany/homepage/HomeActivity.kt
+++ app/src/main/java/com/sizdev/arkhireforcompany/homepage/HomeActivity.kt
@@ -1,10 +1,10 @@
 package com.sizdev.arkhireforcompany.homepage
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.sizdev.arkhireforcompany.R
 import com.sizdev.arkhireforcompany.databinding.ActivityHomeBinding
